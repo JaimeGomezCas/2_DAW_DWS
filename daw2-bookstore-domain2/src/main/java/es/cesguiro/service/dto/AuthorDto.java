57d401719a08c5e8982c0b55b7ba0f3bcd5bf14f
Jaime Gómez <jaimejgc2018@gmail.com>
2025-10-01 13:49:33 +0200

    estoy hasta la pollla

--- daw2-bookstore-domain2/src/main/java/es/cesguiro/service/dto/AuthorDto.java
+++ daw2-bookstore-domain2/src/main/java/es/cesguiro/service/dto/AuthorDto.java
@@ -1,12 +1,20 @@
 package es.cesguiro.service.dto;
 
+import jakarta.validation.constraints.AssertFalse;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+
 public record AuthorDto(
+        @NotNull(message = "No puede ser nulo")
         String name,
         String nationality,
         String biographyEs,
         String biographyEn,
         int birthYear,
         Integer deathYear,
+        @NotNull(message = "No puede ser nulo")
+        @Pattern(regexp = "^[a-z0-9]+(?:-[a-z0-9]+)*$",
+                message = "Slug inválido, debe ser URL-friendly (minúsculas, números y guiones)")
         String slug
 ) {
 
