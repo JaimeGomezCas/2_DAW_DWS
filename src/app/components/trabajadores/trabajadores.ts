ecc32e7d7cb1d2a16379020ab1659fd3b8f0c47e
HugoMengualAlcGVA <hugmenalc@alu.edu.gva>
2025-10-16 12:33:53 +0200

    Initial Commit

---
+++ src/app/components/trabajadores/trabajadores.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { Trabajador } from "../trabajador/trabajador";
+
+@Component({
+  selector: 'app-trabajadores',
+  imports: [Trabajador],
+  templateUrl: './trabajadores.html',
+  styleUrl: './trabajadores.css'
+})
+export class Trabajadores {
+
+}
